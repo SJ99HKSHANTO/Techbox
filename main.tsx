566e6767db216839e49951319de9840fcd3b9122
SJ99HKSHANTO <shantoray049@gmail.com>
2026-08-31 00:37:06 +0600

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,8 +3,6 @@ import "./firebase.js";
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-import './style.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
